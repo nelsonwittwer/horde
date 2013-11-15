078c118f8f9119c51d634908ad82487715f8dfc7
Nelson Wittwer <nelson.wittwer@moneydesktop.com>
2013-11-14 17:17:30 -0700

    Initial commit after initial user scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Horde::Application.config.secret_key_base = '78131ee00aa671a497775d9a97ca8138253ca2cbb7146f0ae34d39f0eab90fba008977ccaaf5dfb4b41131c54314fa9e5ac711f6d210ef024d06fcd33bd36b85'
